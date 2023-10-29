b36a70ba45404d4434f2173f68aabc579b1a737d
whippyshou <whippyshou@gmail.com>
2023-10-30 01:15:00 +0900

    notificaiton / direct message

--- app/javascript/mastodon/features/notifications/components/notification.jsx
+++ app/javascript/mastodon/features/notifications/components/notification.jsx
@@ -510,9 +510,14 @@ class Notification extends ImmutablePureComponent {
 
   render () {
     const { notification } = this.props;
-    const account          = notification.get('account');
-    const link             = <LinkedDisplayName className='notification__display-name' displayProps={{account, variant: 'simple'}} />;
 
+    if (!notification)
+      return null;
+
+    const account          = notification.get('account');
+    const displayNameHtml  = { __html: account.get('display_name_html') };
+    const link             = <bdi><Link className='notification__display-name' href={`/@${account.get('acct')}`} title={account.get('acct')} to={`/@${account.get('acct')}`} dangerouslySetInnerHTML={displayNameHtml} /></bdi>;
+    
     switch(notification.get('type')) {
     case 'follow':
       return this.renderFollow(notification, account, link);
